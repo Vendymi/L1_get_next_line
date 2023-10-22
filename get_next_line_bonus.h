097e0601c2f505ff7869ed4ed4ac9041a3f287f0
Vendymi <118893652+Vendymi@users.noreply.github.com>
2023-10-22 15:29:43 +0200

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmichali <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 16:16:35 by vmichali          #+#    #+#             */
+/*   Updated: 2023/06/23 16:16:37 by vmichali         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdio.h>
+# include <stddef.h>
+# include <stdlib.h>
+
+char	*ft_free(char *buffer, char *buf);
+char	*ft_next(char *buffer);
+char	*ft_line(char *buffer);
+char	*read_file(int fd, char *res);
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlen(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nitems, size_t size);
+
+#endif
